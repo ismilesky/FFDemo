7312974e85294986898966b8b50b712bd04e1406
ismilesky <fighter_kongfei@126.com>
2017-04-20 10:14:54 +0800

    Initial Commit

---
+++ FFDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFDemo
+//
+//  Created by VS on 2017/4/20.
+//  Copyright © 2017年 FelixKong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FFDemo
+//
+//  Created by VS on 2017/4/20.
+//  Copyright © 2017年 FelixKong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
